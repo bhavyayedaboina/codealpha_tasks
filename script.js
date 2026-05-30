750fde4a3267be773aba9ca64dbd926092e25e8e
Yedaboina <yyedaboinabhavya@gmail.com>
2026-05-30 14:22:21 +0530

    changed cover images

--- script.js
+++ script.js
@@ -1,61 +1,69 @@
-const audio=document.getElementById("audio");
-const playBtn=document.getElementById("play");
-const nextBtn=document.getElementById("next");
-const prevBtn=document.getElementById("prev");
-const playlistBtn=document.getElementById("playlistBtn");
-const cover=document.getElementById("cover");
-const title=document.getElementById("title");
-const progress=document.getElementById("progress");
-const volume=document.getElementById("volume");
-const currentTimeEl=document.getElementById("currentTime");
-const durationEl=document.getElementById("duration");
-const volumeBtn=document.getElementById("volumeBtn");
-const volumePopup=document.querySelector(".volume-popup");
-const favBtn=document.getElementById("favBtn");
-const favoriteList=document.getElementById("favoriteList");
-const artist=document.getElementById("artist");
-let favoriteSongs=[];
+const audio = document.getElementById("audio");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("prev");
+const playlistBtn = document.getElementById("playlistBtn");
 
+const cover = document.getElementById("cover");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+
+const progress = document.getElementById("progress");
+
+const volume = document.getElementById("volume");
+const volumeBtn = document.getElementById("volumeBtn");
+const volumePopup = document.querySelector(".volume-popup");
+
+const currentTimeEl = document.getElementById("currentTime");
+const durationEl = document.getElementById("duration");
+
+const favBtn = document.getElementById("favBtn");
+const favoriteList = document.getElementById("favoriteList");
+
+const playlist = document.getElementById("playlist");
+
+let favoriteSongs = [];
+
+const songs = [
+  {
+    title:"Inthandham song (SitaRamam) 🎶",
+    artist:"SPB Charan",
+    path:"songs/Inthandham.mp3",
+    cover:"images/sitaramam.jpeg"
+   },
    
-//storing all the songs in the array of songs
-const songs=[
+  {
+   title:"I Wanna Be Yours 🎶",
+   artist:"Alex Turner",
+   path:"songs/I-Wanna-Be-Yours.mp3",
+   cover:"images/bhavya.png"
+  },
   
-   {
-     title:"Inthandham song(SitaRamam) 🎶️",
-     artist:"SPB Charan",
-     path:"songs/Inthandham.mp3" ,
-     cover:"images/cute.jpeg"
+  {
+    title:"Kollagotte Song 🎶",
+    artist:"Anirudh Ravichander",
+    path:"songs/Kollagotte.mp3",
+    cover:"images/kollagotte.jpeg"
    },
-   {
-     title:"I wanna be yours song 🎶️",
-     artist:"Alex Turner",
-     path:"songs/I-Wanna-Be-Yours.mp3",
-     cover:"images/bhavya.png"
+   
+  {
+    title:"Adhento Gaani Vunnapaatuga 🎶",
+    artist:"Anirudh Ravichander",
+    path:"songs/Adhento Gaani Vunnapaatuga .mp3",
+    cover:"images/hi_nanna.jpeg"
    },
-    {
-     title:"Kollagotte song 🎶️",
-     artist:"Anirudh Ravichander",
-     path:"songs/Kollagotte.mp3",
-     cover:"images/anju.jpeg"
-   },
-   {
-     title:"Adhento Gaani Vunnapaatuga song 🎶️",
-     artist:"Anirudh Ravichander",
-     path:"songs/Adhento Gaani Vunnapaatuga .mp3",
-     cover:"images/friends.jpeg"
-   },
-   {
-     title:"Amma Amma Neevennela song 🌍️",
-     artist:"Sid Sriram",
-     path:"songs/Amma Amma Nee Vennela.mp3",
-     cover:"images/mom.jpeg"
-   }
-]
-//current song index
-let songIndex=0;
+   
+  {
+    title:"Amma Amma Nee Vennela 🌍",
+    artist:"Sid Sriram",
+    path:"songs/Amma Amma Nee Vennela.mp3",
+    cover:"images/Amma.jpeg"
+  }
+ ];
 
-//song playing state initially false
-let isPlaying=false;
+let songIndex = 0;
+
+let isPlaying = false;
 
 //loading the songs function
 function loadSong(index)
@@ -68,192 +76,215 @@ function loadSong(index)
  updateFavoriteIcon();
 }
 
-//song play time
-function formateSong(time)
+function formatSongTime(time)
 {
-  let minutes=Math.floor(time/60);
-  let seconds=Math.floor(time%60);
-  if(seconds<10)
-  {
-   seconds="0"+seconds;
-  }
-  return minutes+":"+seconds;
-}   
+let minutes = Math.floor(time / 60);
+let seconds = Math.floor(time % 60);
 
-//play song function
+if(seconds < 10)
+{
+seconds = "0" + seconds;
+}
+
+return minutes + ":" + seconds;
+}
+
 function playSong()
 {
-  audio.play();
-  isPlaying=true;
-  playBtn.innerText="⏸️";
-  cover.style.animationPlayState="running";
+audio.play();
+
+isPlaying = true;
+
+playBtn.innerText = "⏸️";
+
+cover.style.animationPlayState = "running";
 }
 
-//pause song function
 function pauseSong()
 {
-  audio.pause();
-  isPlaying=false;
-  playBtn.innerText="◀️"; 
-  cover.style.animationPlayState="paused";
+audio.pause();
+
+isPlaying = false;
+
+playBtn.innerText = "▶️";
+
+cover.style.animationPlayState = "paused";
 }
 
-//toggle song(play or pause songs)
-playBtn.addEventListener("click",function(){
-   if(isPlaying)
-   {
-    pauseSong();
-   }
-   else{
-    playSong();
-    }
-} ); 
+playBtn.addEventListener("click", function(){
 
-//playlist using loops
-const playlist=document.getElementById("playlist");
-songs.forEach(function(song,index){
-    const songDiv=document.createElement("div");
-    songDiv.innerText=song.title;
-    songDiv.classList.add("song-item");
-    songDiv.addEventListener("click",function(){
-    songIndex=index;
-    loadSong(songIndex);
-    playSong();
- }); 
- playlist.appendChild(songDiv);
+if(isPlaying)
+{
+pauseSong();
+}
+else
+{
+playSong();
+}
+
 });
 
-//playlist display
+songs.forEach(function(song,index){
+
+const songDiv = document.createElement("div");
+
+songDiv.classList.add("song-item");
+
+songDiv.innerText = song.title;
+
+songDiv.addEventListener("click",function(){
+
+songIndex = index;
+
+loadSong(songIndex);
+
+playSong();
+
+});
+
+playlist.appendChild(songDiv);
+
+});
+
 playlistBtn.addEventListener("click",function(){
-     if(playlist.style.display==="block")
-     {
-       playlist.style.display="none";
-     }
-     else{
-       playlist.style.display="block";
-      }
- });
-          
-//next song runnning function
+
+if(playlist.style.display === "block")
+{
+playlist.style.display = "none";
+}
+else
+{
+playlist.style.display = "block";
+}
+
+});
+
 function nextSong()
 {
- songIndex++;
- if(songIndex>=songs.length)
- {
-   songIndex=0;
- }
- loadSong(songIndex);
- playSong();
+songIndex++;
+
+if(songIndex >= songs.length)
+{
+songIndex = 0;
 }
 
-//previous song function
+loadSong(songIndex);
+
+playSong();
+}
+
 function prevSong()
 {
- songIndex--;
- if(songIndex <0)
- {
-   songIndex=songs.length-1;
- }
- loadSong(songIndex);
- playSong();
+songIndex--;
+
+if(songIndex < 0)
+{
+songIndex = songs.length - 1;
 }
 
-//next and previous buttons actions
+loadSong(songIndex);
+
+playSong();
+}
+
 nextBtn.addEventListener("click",nextSong);
 prevBtn.addEventListener("click",prevSong);
 
-//update progress bar
 audio.addEventListener("timeupdate",function(){
-    
-    progress.value=(audio.currentTime/audio.duration)*100;
-    
-    currentTimeEl.innerText=formateSong(audio.currentTime);
-    durationEl.innerText=formateSong(audio.duration);
-    
+
+if(audio.duration)
+{
+progress.value =
+(audio.currentTime / audio.duration) * 100;
+}
+
+currentTimeEl.innerText =
+formatSongTime(audio.currentTime);
+
+if(!isNaN(audio.duration))
+{
+durationEl.innerText =
+formatSongTime(audio.duration);
+}
+
 });
 
-//volume control
+progress.addEventListener("input",function(){
+
+audio.currentTime =
+(progress.value * audio.duration) / 100;
+
+});
+
 volume.addEventListener("input",function(){
-      audio.volume=volume.value;
-});  
+
+audio.volume = volume.value;
+
+});
+
 volumeBtn.addEventListener("click",function(){
 
-   if(volumePopup.style.display==="block")
-   {
-      volumePopup.style.display="none";
-   }
+if(volumePopup.style.display === "block")
+{
+volumePopup.style.display = "none";
+}
+else
+{
+volumePopup.style.display = "flex";
+}
 
-   else{
-
-      volumePopup.style.display="block";
-   }
-
-});    
-
+});
 
 function updateFavoriteIcon()
 {
-   let currentSong=songs[songIndex];
+let currentSong = songs[songIndex];
 
-   let found=favoriteSongs.find(function(song){
+let found = favoriteSongs.find(function(song){
 
-      return song.title===currentSong.title;
+return song.title === currentSong.title;
 
-   });
+});
 
-   if(found)
-   {
-      favBtn.innerText="❤️";
-   }
-   else
-   {
-      favBtn.innerText="🤍";
-   }
+if(found)
+{
+favBtn.innerText = "❤️";
 }
-
-
-// FAVORITE BUTTON
+else
+{
+favBtn.innerText = "🤍";
+}
+}
 
 favBtn.addEventListener("click",function(){
 
-   let currentSong=songs[songIndex];
+let currentSong = songs[songIndex];
 
-   let alreadyAdded=favoriteSongs.find(function(song){
+let alreadyAdded = favoriteSongs.find(function(song){
 
-      return song.title===currentSong.title;
-
-   });
-
-   if(!alreadyAdded)
-   {
-      favoriteSongs.push(currentSong);
-
-      const favSong=document.createElement("div");
-
-      favSong.classList.add("favorite-song");
-
-      favSong.innerText=currentSong.title+" 🎶";
-
-      favoriteList.appendChild(favSong);
-   }
-
-   updateFavoriteIcon();
+return song.title === currentSong.title;
 
 });
 
- 
-//seek song
-progress.addEventListener("input",function(){
-   audio.currentTime=(progress.value*audio.duration)/100;
+if(!alreadyAdded)
+{
+favoriteSongs.push(currentSong);
+
+const favSong = document.createElement("div");
+
+favSong.classList.add("favorite-song");
+
+favSong.innerText = currentSong.title + " 🎶";
+
+favoriteList.appendChild(favSong);
+}
+
+updateFavoriteIcon();
+
 });
 
-//auto play song
 audio.addEventListener("ended",nextSong);
 
-//initiallly set one song
-loadSong(songIndex);
+audio.volume = 0.5;
+volume.value = 0.5;
 
-    
-      
-       
-     
+loadSong(songIndex);
